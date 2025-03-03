b7762d2e8b5cdebfb6ce5390a8af09dc1c858ce2
ShirNegev <shirbdc@gmail.com>
2025-03-03 16:27:07 +0200

    feat(header): Added header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './components/navbar'
 import LoginPage from './pages/login-page'
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <LoginPage></LoginPage>
     </>
   )
