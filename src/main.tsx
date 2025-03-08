d8a009d26b2512016626597f9b5847256f8c46e9
ShirNegev <shirbdc@gmail.com>
2025-03-08 23:31:55 +0200

    feat(google-auth): add google auth logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import App from './App.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <GoogleOAuthProvider clientId="737068141049-0d4o1tjsld1meirsqqj66c1e7u3mh6u2.apps.googleusercontent.com">
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
+);
